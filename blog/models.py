d33b75227b8726eb0f64c95822772c18638cbd65
Tes3103 <>
2023-10-21 18:47:26 +0000

    Add class content for comment in model.py

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,8 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -21,14 +22,15 @@ class Post(models.Model):
         ordering = ['-created_on']
 
     def _str_(self):
-        return self.title 
+        return self.title
 
     def number_of_likes(self):
         return self.likes.count()
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=100)
     email = models.EmailField()
     body = models.TextField()
